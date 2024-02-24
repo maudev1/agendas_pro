b2d9e1eb83da51dd0c15131f8d8eb004896a0dd2
mauriciojr <mauriciojr.dev@gmail.com>
2024-02-24 07:55:51 -0300

    Traduções para portugues

---
+++ resources/lang/pt/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'failed' => 'Estas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+    
+
+];
