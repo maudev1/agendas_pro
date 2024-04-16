8fe3d37451a73d31933bced95d6d74c13994a824
mauriciojr <mauriciojr.dev@gmail.com>
2024-04-15 21:27:52 -0300

    Assign Rule to users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+
 
 
 class DatabaseSeeder extends Seeder
@@ -16,15 +20,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        return DB::table('users')->insert([
+
+        $user =  new User();
+        
+        $user->insert([
             'name' => 'admin',
-            'email' => 'mauriciojr.dev@gmail.com',
+            'email' => 'admin@agendaspro.com',
             'document' => '3333333333',
             'phone' => '11996502162',
             'password' => Hash::make('juniorsk8')
         ]);
 
-        
+        // Create Permissions
+
+        $permissions = new PermissionSeeder();
+        $permissions->run();
+
+        // Create Rules
+
+        $roles = new RoleSeeder();
+        $roles->run();
+
+        // Add Rule to User admin
+
+        $user->assignRole('admin');
 
     }
+
 }
